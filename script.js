7e40f0b0d1dc1c564e23724e38e45d871c3a89cb
j-goodman <johncurtisgoodman@gmail.com>
2024-12-17 17:53:39 -0500

    Initial commit.

---
+++ script.js
@@ -0,0 +1,56 @@
+const navbarScript = () => {
+    const homepageButton = document.getElementById("homepage-button")
+    const journalismButton = document.getElementById("journalism-button")
+    const theaterButton = document.getElementById("theater-button")
+    
+    const mobileHomepageButton = document.getElementById("mobile-homepage-button")
+    const mobileJournalismButton = document.getElementById("mobile-journalism-button")
+    const mobileTheaterButton = document.getElementById("mobile-theater-button")
+    
+    const homepagePage = document.getElementById("homepage-page")
+    const journalismPage = document.getElementById("journalism-page")
+    const theaterPage = document.getElementById("theater-page")
+    
+    const menuButton = document.getElementById("menu-button")
+    const mobileMenu = document.getElementById("mobile-menu")
+    
+    const pages = [
+        homepagePage,
+        journalismPage,
+        theaterPage
+    ]
+
+    homepageButton.onclick = mobileHomepageButton.onclick = () => {
+        clearPages()
+        homepagePage.classList.add("visible")
+        mobileMenu.classList.add("nondisplay")
+    }
+ 
+    journalismButton.onclick = mobileJournalismButton.onclick = () => {
+        clearPages()
+        journalismPage.classList.add("visible")
+        mobileMenu.classList.add("nondisplay")
+    }
+ 
+    theaterButton.onclick = mobileTheaterButton.onclick = () => {
+        clearPages()
+        theaterPage.classList.add("visible")
+        mobileMenu.classList.add("nondisplay")
+    }
+
+    menuButton.onclick = () => {
+        if (mobileMenu.className.includes("nondisplay")) {
+            mobileMenu.classList.remove("nondisplay")
+        } else {
+            mobileMenu.classList.add("nondisplay")
+        }
+    }
+    
+    const clearPages = () => {
+        pages.forEach(page => {
+            page.classList.remove("visible")
+        })
+    }
+}
+
+navbarScript()
